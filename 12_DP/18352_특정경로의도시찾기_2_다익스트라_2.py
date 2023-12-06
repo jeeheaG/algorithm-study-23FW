fa7466a3e9723d3ba239b8e79c2c4add6d32101a
jeeheaG <jeehea99@naver.com>
2023-12-06 18:44:51 +0900

    18352 코드개선

--- 12_DP/18352_특정경로의도시찾기_2_다익스트라_2.py
+++ 12_DP/18352_특정경로의도시찾기_2_다익스트라_2.py
@@ -1,18 +1,19 @@
+# https://www.acmicpc.net/problem/18352
 # 최단거리 ->bfs->큐deque / 또는 ->다익->우선순위큐heapq, 그래프
 from sys import stdin
 import heapq
 input = stdin.readline #입력방식대체
 
 ## 입력
-n, m, k, x = map(int, input().split())
+n, m, ans_dist, start = map(int, input().split())
 
 #그래프 노드번호 인덱싱 쉽게 +1
 graph = [[] * (n+1) for _ in range(n+1)]
 
 #경로 입력받기 : heapq에 넣기 편하게 튜플로 경로 저장(2차원배열이나 딕셔너리로 안하고)
 for _ in range(m) :
-    start, end = map(int, input().split())
-    graph[start].append((1, end)) #가중치가 모두 동일하므로 1로 통일
+    sn, en = map(int, input().split())
+    graph[sn].append((1, en)) #가중치가 모두 동일하므로 1로 통일
 
 # print(*graph, sep="\n")
 
@@ -21,8 +22,8 @@ dist_arr = [float("INF")] * (n+1)
 
 # 우선순위큐와 시작점 설정
 que = []
-heapq.heappush(que, (0, x)) # 시작노드는 현재노드이므로 거리 = 0
-dist_arr[x] = 0 # 시작노드 최단거리값 갱신해주고 시작함
+heapq.heappush(que, (0, start)) # 시작노드는 현재노드이므로 거리 = 0
+dist_arr[start] = 0 # 시작노드 최단거리값 갱신해주고 시작함
 
 ## 다익스트라
 while que :
@@ -44,12 +45,11 @@ while que :
             heapq.heappush(que, (cost, nxt))
 
 ## 출력
-# print(*dist_arr)
-isExist = False
-for i, node in enumerate(dist_arr[1:]) :
-    if node == k :
-        print(i+1)
-        isExist = True
+ans = [i for i, d in enumerate(dist_arr) if d==ans_dist]
+print(*ans if ans else [-1], sep="\n")
 
-if not isExist :
-    print(-1)
+# 마지막줄은 이거랑 같은뜻.. 함 줄여봄
+# if ans :
+#     print(*ans, sep="\n")
+# else :
+#     print(-1)
